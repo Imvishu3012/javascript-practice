8741017c99c281e7cbaea71ae532d319734181c5
Imvishu3012 <154280045+Imvishu3012@users.noreply.github.com>
2025-01-31 05:22:33 +0530

    Update youtubeasync.js

--- youtubeasync.js
+++ youtubeasync.js
@@ -1,14 +1,14 @@
 const p1 = new Promise((resolve,reject)=>{setTimeout(()=>resolve('you are the first promise'),10000)})
 
 const p2 = new Promise ((resolve, reject)=>{
-    setTimeout(()=>resolve('you are the second promise'),5000)
+    setTimeout(()=>resolve('you are the second promise'),5000);
 })
 
 async function twopromise(){
-    console.log('Hello World')
+    console.log('Hello World');
     const val1 = await p1
     console.log("Namaste Javascript1")
-    console.log(val1)
+    console.log(val1);
     
     const val2 = await p2
     console.log('Namaste Javascript2');
